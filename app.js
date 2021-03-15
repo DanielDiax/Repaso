d7fdc13aad7a0468758af9d2f9de1b35977e8a19
DanielDiax <71106606+DanielDiax@users.noreply.github.com>
2021-03-15 00:00:56 -0500

    Repaso.js

--- app.js
+++ app.js
@@ -1,5 +1,71 @@
 let myArray = [2,3,4,5,6,7];
+
+function getData(data){
+    return new Promise((resolve,reject)=>{
+        setInterval(()=>{
+            resolve(data);
+        },2000);
+    });
+    
+}
+getData(myArray).then((cosa)=>{
+    console.log("Cumplí la promesa");
+    console.log("Hola" + cosa);
+})
+
+//45 min 46 seg de la clase 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //(function name parameters)
+function test(){
+    console.log("Hola")
+}
+
+const test1 = (a) => {      //-> funsiones flecha
+    console.log("Hola")
+    return a;
+}
+let myData = test1(3);
+
+
+let aux = 0;
+myArray.forEach((element) => {  // -> funsiones flecha con un Callback
+    aux = aux + element;
+});
+
+let myNewArray = myArray.map((element) => {
+
+    return element*4;//[8,16,20,24,28]
+})
+console.log(myNewArray);*/
+
+//-----------------------------------------------------------------------------------
+
+/*
 function average (data){
     let aux = 0
     for(element of data)
@@ -11,4 +77,4 @@ function average (data){
 }
 
 let result = average(myArray);
-console.log(result);
+console.log(result);*/
