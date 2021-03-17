0c1f4c4ff134d97dca0bdc10185221d4e96e6536
DanielDiax <71106606+DanielDiax@users.noreply.github.com>
2021-03-17 16:25:06 -0500

    Repaso JS
    Consumo de API
    operador ternario
    Promesas
    Funciones flecha

--- app.js
+++ app.js
@@ -1,34 +1,93 @@
-let myArray = [2,3,4,5,6,7];
+const fetchData = async () => {
+    let response = await fetch("http://newsapi.org/v2/top-headlines?country=co&category=business&apiKey=abae27f412a544b1b70781ecc0ccad02");
+    let dataJson = await response.json();
+    return dataJson.articles;
+}
+
+const renderData = async () => {
+    const articles = await fetchData();
+    let container = document.getElementById("articles");
+    let urlImageNotFound = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.publicdomainpictures.net%2Fes%2Fview-image.php%3Fimage%3D270609%26picture%3Dimagen-no-encontrada&psig=AOvVaw25Y6j33WIk3_0ykUFjbbIQ&ust=1616100590816000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi7qYCauO8CFQAAAAAdAAAAABAI";
+    // Operador Ternario // Condicion ? instruccion : instruccion
+    articles.forEach((article) => {
+        let alertTemplate = `<div class="col-12 col-sm-12 col-md-6 col-lg-4">
+                                <div class="card" style="width: 18rem; ">
+                                <img src="${article.urlToImage == null ? urlImageNotFound : article.urlToImage}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                    <h5 class="card-title">${article.title}</h5>
+                                    <p class="card-text">${article.description == null ? "Descripión no disponible" : article.description}</p>
+                                    </div>
+                                </div>
+                            </div>`;
+        container.innerHTML += alertTemplate;
+    });
+}
+renderData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let myArray = [2,3,4,5,6,7];
 
 function getData(data){
     return new Promise((resolve,reject)=>{
+        if(data.length == 0){
+            reject(new error("Hubo un error"));  //Codigo java Script que se ejecuta a futuro (Promesas)
+        }
         setInterval(()=>{
             resolve(data);
         },2000);
     });
-    
-}
-getData(myArray).then((cosa)=>{
+
+}*/
+
+/*getData(myArray).then((cosa)=>{
     console.log("Cumplí la promesa");
-    console.log("Hola" + cosa);
-})
+    console.log("Hola" + cosa);         // Forma antigua de cumplir promesa
+}).catch((error)=>{
+    console.log("Error");
+});*/
 
-//45 min 46 seg de la clase 
+/*async function myFunction(){
+    let myData = await getData(myArray);  //Cumplir promesa con funcion normal
+    console.log(myData);
+}*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*const fetchData = async () => {
+    let myData = await getData(myArray);  //Cumplir promesa con funcion flecha
+    console.log(myData);
+}
+fetchData();*/
 
 
 
